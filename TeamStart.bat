38856a8bf0314fcea46b6f3a0881cf690b92448b
fahim-a <f.abdun.nur@gmail.com>
2014-03-23 14:54:29 -0400

    Added Windoze (sic) batch file

---
+++ TeamStart.bat
@@ -0,0 +1,20 @@
+start java -cp ../bin;./lib/weka.jar Krislet -team Carleton;
+ping localhost
+start java -cp ../bin;./lib/weka.jar Krislet -team Carleton;
+REM ping localhost
+start java -cp ../bin;./lib/weka.jar Krislet -team Carleton;
+REM ping localhost
+start java -cp ../bin;./lib/weka.jar Krislet -team Carleton;
+REM ping localhost
+start java -cp ../bin;./lib/weka.jar Krislet -team Carleton;
+REM ping localhost
+start java -cp ../bin;./lib/weka.jar Krislet -team uOttawa;
+REM ping localhost
+start java -cp ../bin;./lib/weka.jar Krislet -team uOttawa;
+REM ping localhost
+start java -cp ../bin;./lib/weka.jar Krislet -team uOttawa;
+REM ping localhost
+start java -cp ../bin;./lib/weka.jar Krislet -team uOttawa;
+REM ping localhost
+start java -cp ../bin;./lib/weka.jar Krislet -team uOttawa;
+REM ping localhost
