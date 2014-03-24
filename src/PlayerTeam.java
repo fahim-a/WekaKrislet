0a1150ebe48b47d545bde35762301c57323d82b7
fahim-a <f.abdun.nur@gmail.com>
2014-03-24 13:01:34 -0400

    Minor javadoc changes

--- src/PlayerTeam.java
+++ src/PlayerTeam.java
@@ -1,3 +1,7 @@
+/**
+ * Enumerates the teams an agent is playing for.
+ * 
+ */
 public enum PlayerTeam {
     FRIEND, FOE
 }
--- src/SoccerAction.java
+++ src/SoccerAction.java
@@ -1,4 +1,6 @@
 /**
+ * Defines the set of actions our soccer agent can perform
+ * 
  * Note: Ensure these properties are defined in the exact same order in the
  * properties file
  * 
